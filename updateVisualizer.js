a9bd5f4d50e621c9d8fbd26ca08ceed069db5bb6
JaxJosiah <46610575+JaxJosiah@users.noreply.github.com>
2019-03-23 13:56:51 -0700

    Improved display and UI performance

--- updateVisualizer.js
+++ updateVisualizer.js
@@ -1,7 +1,30 @@
 function updateVisualizer()
 {
+    // Displays Vx's
     for (let i = "0"; i <= 15; i++)
     {
-        document.getElementById("V" + i).innerHTML = "V" + i + ": " + Processor.Registers[i];
+        document.getElementById("V" + i).innerHTML = "<b>V" + i + ":</b> " + Processor.Registers[i]; // Maybe I should use CSS to make the text bold
     }
+
+    // Displays the memory
+    var memorylist = "\n";
+    for (let i = 0; i <= 4095; i++)
+    {
+        memorylist += " " + i + ": \t" + Processor.Memory[i] + "\n";
+    }
+    document.getElementById("MEMORY").value = memorylist;
+
+    // Displays the PC, VI, DT, and ST
+    var misc = "<b>PC:</b> " + Processor.PC;
+    misc += "\n\n<b>VI:</b> " + Processor.ISpecial;
+    misc += "\n\n<b>Delay Timer:</b> " + Processor.delayTimer;
+    if (Processor.soundTimer < 0)
+    {
+        misc += "\n\n<b>Sound Timer:</b> " + 0;
+    }
+    else
+    {
+        misc += "\n\n<b>Sound Timer:</b> " + Processor.soundTimer;
+    }
+    document.getElementById("MISC").innerHTML = misc;
 }
